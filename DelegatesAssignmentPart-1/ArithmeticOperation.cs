b7614ead2775d3a069a4685a5a6bc989b13383a9
Maheshvema <babumahesh2088@gmail.com>
2023-11-27 15:40:33 +0530

    modified

--- DelegatesAssignmentPart-1/ArithmeticOperation.cs
+++ DelegatesAssignmentPart-1/ArithmeticOperation.cs
@@ -6,12 +6,32 @@ using System.Threading.Tasks;
 
 namespace DelegatesAssignmentPart_1
 {
-    internal delegate int ArithmeticOperation(int x, int y);
+    internal delegate int ArithmeticOperation(int a, int b);
+
+    
     internal class Arithmeticoperation
     {
+        static int Add(int a, int b) => a + b;
+        static int Subtract(int a, int b) => a - b;
+        static int Multiply(int a, int b) => a * b;
+        static int Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return 0;
+            }
+        }
         static void Main(string[] args)
         {
-            ArithmeticOperation operation  ;
+            ArithmeticOperation addOperation = Add;
+            ArithmeticOperation subtractOperation = Subtract;
+            ArithmeticOperation multiplyOperation = Multiply;
+            ArithmeticOperation divideOperation = Divide;
             Console.WriteLine("Enter the first number:");
             int num1=int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Second number:");
@@ -19,26 +39,31 @@ namespace DelegatesAssignmentPart_1
             Console.WriteLine("Enter the user option to operation:");
             Console.WriteLine("1.Add 2.sub 3.mul 4.Div");
             int op=int.Parse(Console.ReadLine());
+            int result;
             switch(op)
             {
                 case 1:
                     {
-                        operation = (a, b) => a + b;
+                        result = addOperation(num1, num2);
+                        Console.WriteLine($"Result: {num1} + {num2} = {result}");
                         break;
                     }
                 case 2:
                     {
-                       operation = (a, b) => a - b;
+                        result = subtractOperation(num1, num2);
+                        Console.WriteLine($"Result: {num1} - {num2} = {result}");
                         break;
                     }
                 case 3:
                     {
-                       operation = (a, b) => a * b;
+                        result = multiplyOperation(num1, num2);
+                        Console.WriteLine($"Result: {num1} * {num2} = {result}");
                         break;
                     }
                 case 4:
                     {
-                       operation = (a, b) => a / b;
+                        result = divideOperation(num1, num2);
+                        Console.WriteLine($"Result: {num1} + {num2} = {result}");
                         break;
                     }
                 default:
@@ -47,8 +72,6 @@ namespace DelegatesAssignmentPart_1
                         return;
                     }
             }
-            int result = operation(num1, num2);
-            Console.WriteLine($"Result: {result}");
             Console.ReadKey();           
         }
     }
